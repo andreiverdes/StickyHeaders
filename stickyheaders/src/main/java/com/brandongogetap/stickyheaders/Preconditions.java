9080288100c70ee6786995ab71105810b6ba1e13
Andrei <office@andreiverdes.com>
2017-08-31 03:57:51 +0200

    Adds support for DrawerLayout

--- stickyheaders/src/main/java/com/brandongogetap/stickyheaders/Preconditions.java
+++ stickyheaders/src/main/java/com/brandongogetap/stickyheaders/Preconditions.java
@@ -1,6 +1,7 @@
 package com.brandongogetap.stickyheaders;
 
 import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -19,7 +20,9 @@ final class Preconditions {
 
     static void validateParentView(View recyclerView) {
         View parentView = (View) recyclerView.getParent();
-        if (!(parentView instanceof FrameLayout) && !(parentView instanceof CoordinatorLayout)) {
+        if (!(parentView instanceof FrameLayout)
+                && !(parentView instanceof CoordinatorLayout)
+                && !(parentView instanceof DrawerLayout)) {
             throw new IllegalArgumentException("RecyclerView parent must be either a FrameLayout or CoordinatorLayout");
         }
     }
